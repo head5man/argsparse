cb138a1dea334df574a3d671420b492cbf0699a1
Tuomas Lahtinen <tuomas123lahtinen@gmail.com>
2025-02-22 12:39:04 +0200

    Refactor example application
    Move initialization code to helper function.
    Convert to cplusplus to utilize exceptions.
    Use argsparse API to print argument values.

--- example/example.cpp
+++ example/example.cpp
@@ -2,19 +2,36 @@
 
 #include <stdio.h>
 
-int err_print(ARG_ERROR err, const char* message);
+int gFlag = 0;
+
+int err_print(int err, const char *message);
+
+void initialize_arguments(ARG_DATA_HANDLE arguments);
 
 void arg_print(ARG_ARGUMENT_HANDLE arg);
 
 int main(int argc, char **argv)
 {
-    for (int i = 0; i < argc; i++)
-    {
-        printf("argv[%d]: %s terminator: %d\n", i, argv[i], (i == 0) ? 0 : *(argv[i] - 1));
-    }
-
     ARG_DATA_HANDLE arguments = argsparse_create("argsparse-example");
+    try
+    {
+        initialize_arguments(arguments);
+        int parsed = argsparse_parse_args(arguments, argv, argc);
+        printf("shortopts %s - %d arguments parsed\n", argsparse_get_shortopts(arguments), parsed);
+        argsparse_show_arguments(arguments);
+    }
+    catch (...)
+    {
+        printf("exit due to exception");
+    }
+    argsparse_free(arguments);
+}
+
+void initialize_arguments(ARG_DATA_HANDLE arguments)
+{
     ARG_ERROR err = ERROR_NONE;
+    err = argsparse_add_help(arguments);
+    err_print(err, "help not added");
     err = argsparse_add_int(arguments, "integer", "This is an integer value", 0);
     err_print(err, "integer not added");
 
@@ -24,24 +41,18 @@ int main(int argc, char **argv)
     err = argsparse_add_cstr(arguments, "string", "This is a string value", "");
     err_print(err, "string not added");
 
-    err = argsparse_add_flag(arguments, "flag", "This is a flag value", 0);
+    err = argsparse_add_flag(arguments, "flag", "This is a flag value", 123, &gFlag);
     err_print(err, "flag not added");
-
-    int parsed = argsparse_parse_args(arguments, argv, argc);
-    printf("shortopts %s - %d arguments parsed\n", argsparse_get_shortopts(arguments), parsed);
-    ARG_ARGUMENT_HANDLE arg = NULL;
-    arg = argsparse_argument_by_short_name(arguments, 'i');
-    arg_print(arg);
-
-    arg = argsparse_argument_by_short_name(arguments, 's');
-    arg_print(arg);
 }
 
-int err_print(ARG_ERROR err, const char* message)
+int err_print(int err, const char* message)
 {
     if (err != ERROR_NONE)
-        return printf("ERR(%d): %s\n", err, message);
-    return 0;
+    {
+        printf("ERR(%d): %s\n", err, message);
+        throw err;
+    }
+    return err;
 }
 
 void arg_print(ARG_ARGUMENT_HANDLE arg)
@@ -64,7 +75,7 @@ void arg_print(ARG_ARGUMENT_HANDLE arg)
                 printf(str_fmt, arg->name, arg->name_short, arg->value.stringvalue);
                 break;
             case ARGSPARSE_TYPE_FLAG:
-                printf(flg_fmt, arg->name, arg->name_short, arg->value.flagvalue);
+                printf(flg_fmt, arg->name, arg->name_short, *arg->value.flagptr);
                 break;
             default:
                 printf("Unsupported type");
